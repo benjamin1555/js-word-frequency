a17b5a5e9311e35c60d9fe3bee432a346b5a068a
Benjamin Teste <benjamin.teste@gmail.com>
2021-01-14 16:06:31 -0500

    add README

---
+++ README.md
@@ -0,0 +1,4 @@
+# Word Frequency Counter
+A simple word frequency counter built using vanilla JS.
+
+[https://word-frequency-app.herokuapp.com/](Click here) to try it.
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './style/main.scss';
 import { wordFrequency } from './word-frequency.js';
 import { initChartJs } from './chart.js';
 
-
 document.querySelector('.input-area__submit').addEventListener('click', () => {
   const wordsCount = wordFrequency();
 
